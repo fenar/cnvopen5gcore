182cf014afdba881d14ddec402cfb3bb19f81c0d
Fatih Nar <fenar@yahoo.com>
2021-04-23 19:57:45 -0500

    Update 0-deploy5gcore.sh

--- 0-deploy5gcore.sh
+++ 0-deploy5gcore.sh
@@ -5,13 +5,6 @@ echo
 oc adm policy add-scc-to-user anyuid -z default
 oc adm policy add-scc-to-user hostaccess -z default
 oc adm policy add-scc-to-user hostmount-anyuid -z default
-#echo -e "Creating the needed certificates....\n"
-#echo
-#cd ca-tls-certificates
-#oc create secret -n open5gs generic mongodb-ca --from-file=rds-combined-ca-bundle.pem
-#echo
-#oc get secret -n open5gs
-#pwd
 cd helm-chart
 oc create -f upfhd1-pvc.yaml
 echo "Deploying Open5Gs"
