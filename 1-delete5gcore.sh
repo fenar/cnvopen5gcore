60dce15303857ff631ca3a14d26760b8a73468bc
Fatih Nar <fenar@yahoo.com>
2021-04-26 11:54:05 -0500

    Update 1-delete5gcore.sh

--- 1-delete5gcore.sh
+++ 1-delete5gcore.sh
@@ -2,8 +2,9 @@
 #Author: fenar
 echo -e "World is About to End!....\n"
 echo
+
 cd helm-chart
 echo "Removing Open5Gs"
 helm -n open5gs uninstall 5gcore ./
-oc delete -f upfhd1-pvc.yaml
+#oc delete -f upfhd1-pvc.yaml
 echo "The End"
