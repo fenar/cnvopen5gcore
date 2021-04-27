07082800862bd045be89990d77d7294c6c72dc13
Fatih Nar <fenar@yahoo.com>
2021-04-26 19:47:49 -0500

    Update 1-deployUERANSIM.sh

--- 1-deployUERANSIM.sh
+++ 1-deployUERANSIM.sh
@@ -5,5 +5,6 @@ echo
 oc project open5gs
 cd ueransim-helm
 echo "Deploying UERANSIM"
+sed -e "s/<put-your-amf-service-ip-here>/$(<amf-ip sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' | tr -d '\n')/g" -i ueransim-gnb-configmap.yaml
 helm -n open5gs install -f values.yaml 5gcore-ue ./
 echo "Enjoy The UERANSIM!"
