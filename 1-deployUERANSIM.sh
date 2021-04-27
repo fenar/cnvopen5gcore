39b658abbab9b1f909e2e369b94881f1a830e39d
Fatih Nar <fenar@yahoo.com>
2021-04-27 08:31:46 -0500

    Update 1-deployUERANSIM.sh

--- 1-deployUERANSIM.sh
+++ 1-deployUERANSIM.sh
@@ -2,7 +2,10 @@
 #Author: fenar
 oc project open5gs
 cd ueransim-helm
-echo "Deploying UERANSIM"
+echo "Preparing gNB config"
+oc get services | grep amf-open5gs-sctp | awk '{print $3}' > amf-ip
+echo "AMF IP:" && cat amf-ip
 sed -e "s/<put-your-amf-service-ip-here>/$(<amf-ip sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' | tr -d '\n')/g" -i ueransim-gnb-configmap.yaml
-helm -n open5gs install -f values.yaml 5gcore-ue ./
+echo "gNB Config:" && cat ueransim-gnb-configmap.yaml
+helm -n open5gs install -f values.yaml ueransim ./
 echo "Enjoy The UERANSIM!"
