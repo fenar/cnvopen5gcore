534034ef5b15ce3e43bd459e148037d8a226d33b
Fatih Nar <fenar@yahoo.com>
2021-04-27 10:58:21 -0500

    Update 1-deployUERANSIM.sh

--- 1-deployUERANSIM.sh
+++ 1-deployUERANSIM.sh
@@ -13,13 +13,14 @@ echo "AMF IP:" && cat amf-ip
 sed -e "s/<put-your-amf-service-ip-here>/$(<amf-ip sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' | tr -d '\n')/g" -i ueransim-gnb-configmap.yaml
 echo "gNB Config:" && cat ueransim-gnb-configmap.yaml
 oc create -f ueransim-gnb-configmap.yaml
-oc create -f ueransim-gnb-deploy.yaml
+#oc create -f ueransim-gnb-deploy.yaml
 ## UE Section
-echo "Preparing UE config"
-oc get services | grep ueransim-gnb | awk '{print $3}' > gnb-ip
-echo "gNB IP:" && cat gnb-ip
-sed -e "s/<put-your-gnb-service-ip-here>/$(<gnb-ip sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' | tr -d '\n')/g" -i ueransim-ue-configmap.yaml
-echo "UE Config:" && cat ueransim-ue-configmap.yaml
+#echo "Preparing UE config"
+#oc get services | grep ueransim-gnb | awk '{print $3}' > gnb-ip
+#echo "gNB IP:" && cat gnb-ip
+#sed -e "s/<put-your-gnb-service-ip-here>/$(<gnb-ip sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' | tr -d '\n')/g" -i ueransim-ue-configmap.yaml
+#echo "UE Config:" && cat ueransim-ue-configmap.yaml
 oc create -f ueransim-ue-configmap.yaml
-oc create -f ueransim-ue-deploy.yaml
+#oc create -f ueransim-ue-deploy.yaml
+oc create -f ueransim-deploy.yaml
 echo "Enjoy The UERANSIM!"
