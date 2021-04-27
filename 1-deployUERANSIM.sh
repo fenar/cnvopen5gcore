4390d2fb4edbb1c77f57cc8c780eb1a283408fa6
Fatih Nar <fenar@yahoo.com>
2021-04-27 11:47:47 -0500

    Update 1-deployUERANSIM.sh

--- 1-deployUERANSIM.sh
+++ 1-deployUERANSIM.sh
@@ -13,6 +13,6 @@ echo "AMF IP:" && cat amf-ip
 sed -e "s/<put-your-amf-service-ip-here>/$(<amf-ip sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' | tr -d '\n')/g" -i ueransim-gnb-configmap.yaml
 echo "gNB Config:" && cat ueransim-gnb-configmap.yaml
 oc create -f ueransim-gnb-configmap.yaml
-#oc create -f ueransim-ue-configmap.yaml
+oc create -f ueransim-ue-configmap.yaml
 oc create -f ueransim-deploy.yaml
 echo "Enjoy The UERANSIM!"
