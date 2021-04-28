31b6f7448c76e8ae62fc6958a1d380b7774b4a39
Fatih Nar <fenar@yahoo.com>
2021-04-28 10:42:57 -0500

    Update 0-deploy5gcore.sh

--- 0-deploy5gcore.sh
+++ 0-deploy5gcore.sh
@@ -6,7 +6,11 @@ oc adm policy add-scc-to-user anyuid -z default
 oc adm policy add-scc-to-user hostaccess -z default 
 oc adm policy add-scc-to-user hostmount-anyuid -z default 
 oc adm policy add-scc-to-user privileged -z default 
+current_dir=$PWD
 cd 5gcore-helm
-echo "Deploying Open5Gs"
+echo "Deploying Open5G Core"
 helm install -f values.yaml 5gcorenew ./
+cd $current_dir
+echo "Creating Istio Ingress Virtual Service"
+oc create -f istio-manifests/open5gsweb.yaml
 echo "Enjoy The Open 5G Core!"
