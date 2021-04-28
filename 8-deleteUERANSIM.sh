101a1daca307f5733f9a7154ec38e3c814549661
Fatih Nar <fenar@yahoo.com>
2021-04-28 08:28:34 -0500

    Update 8-deleteUERANSIM.sh

--- 8-deleteUERANSIM.sh
+++ 8-deleteUERANSIM.sh
@@ -4,7 +4,6 @@ echo -e "Wiping UERANSIM....\n"
 echo
 cd ueransim
 echo "Removing UERANSIM Deployment"
-#helm -n open5gs uninstall ueransim
 oc delete -f ueransim-gnb-configmap.yaml
 oc delete -f ueransim-ue-configmap.yaml
 #oc delete -f ueransim-gnb-deploy.yaml
