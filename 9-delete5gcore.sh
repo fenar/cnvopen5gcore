8dfbca52dda0fc9f1acbd1dbe1fafd1628f579b3
Fatih Nar <fenar@yahoo.com>
2021-04-28 10:44:29 -0500

    Update 9-delete5gcore.sh

--- 9-delete5gcore.sh
+++ 9-delete5gcore.sh
@@ -2,6 +2,8 @@
 #Author: fenar
 echo -e "World is About to End!....\n"
 echo
+echo "Creating Istio Ingress Virtual Service"
+oc delete -f istio-manifests/open5gsweb.yaml
 cd 5gcore-helm
 echo "Removing Open5Gs"
 helm uninstall 5gcorenew
