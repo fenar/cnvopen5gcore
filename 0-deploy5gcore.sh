4d2e50c019721fb65fc95e46faeaa2b3bef2911c
Fatih Nar <fenar@yahoo.com>
2021-04-28 08:27:49 -0500

    Update 0-deploy5gcore.sh

--- 0-deploy5gcore.sh
+++ 0-deploy5gcore.sh
@@ -2,12 +2,12 @@
 #Author: fenar
 echo -e "Configuring privileged access - Sorry it is what is....\n"
 echo
-oc project open5gs
+oc project open5gcore
 oc adm policy add-scc-to-user anyuid -z default -n open5gs
 oc adm policy add-scc-to-user hostaccess -z default -n open5gs
 oc adm policy add-scc-to-user hostmount-anyuid -z default -n open5gs
 oc adm policy add-scc-to-user privileged -z default -n open5gs
 cd 5gcore-helm
 echo "Deploying Open5Gs"
-helm -n open5gs install -f values.yaml 5gcore ./
-echo "Enjoy The 5G Core!"
+helm -n open5gcore install -f values.yaml 5gcore ./
+echo "Enjoy The Open 5G Core!"
