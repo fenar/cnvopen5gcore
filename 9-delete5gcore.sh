32b831032b124722f9a28cf0f4057221f05c09d3
Fatih Nar <fenar@yahoo.com>
2021-04-28 08:28:55 -0500

    Update 9-delete5gcore.sh

--- 9-delete5gcore.sh
+++ 9-delete5gcore.sh
@@ -2,8 +2,7 @@
 #Author: fenar
 echo -e "World is About to End!....\n"
 echo
-
 cd 5gcore-helm
 echo "Removing Open5Gs"
-helm -n open5gs uninstall 5gcore
+helm -n open5gcore uninstall 5gcore
 echo "The End"
