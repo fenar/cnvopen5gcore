d9c7383d65c93e54f85944614c82858a8d730a4e
Fatih Nar <fenar@yahoo.com>
2021-04-29 14:03:22 -0500

    Update 0-deploy5gcore.sh

--- 0-deploy5gcore.sh
+++ 0-deploy5gcore.sh
@@ -2,10 +2,10 @@
 #Author: fenar
 echo -e "Configuring privileged access - Sorry it is what is....\n"
 echo
-oc adm policy add-scc-to-user anyuid -z default 
-oc adm policy add-scc-to-user hostaccess -z default 
-oc adm policy add-scc-to-user hostmount-anyuid -z default 
-oc adm policy add-scc-to-user privileged -z default 
+#oc adm policy add-scc-to-user anyuid -z default 
+#oc adm policy add-scc-to-user hostaccess -z default 
+#oc adm policy add-scc-to-user hostmount-anyuid -z default 
+#oc adm policy add-scc-to-user privileged -z default 
 current_dir=$PWD
 oc create secret generic mongodb-ca --from-file=$current_dir/5gcore-helm/ca-tls-certificates/rds-combined-ca-bundle.pem
 oc get secret
