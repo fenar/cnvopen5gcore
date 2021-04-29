612fb75c4cc79ea30d1df8381a2488aff30ec379
Fatih Nar <fenar@yahoo.com>
2021-04-29 13:21:05 -0500

    Update 0-deploy5gcore.sh

--- 0-deploy5gcore.sh
+++ 0-deploy5gcore.sh
@@ -7,12 +7,9 @@ oc adm policy add-scc-to-user hostaccess -z default
 oc adm policy add-scc-to-user hostmount-anyuid -z default 
 oc adm policy add-scc-to-user privileged -z default 
 current_dir=$PWD
-oc create secret generic mongodb-ca --from-file=$PWD/5gcore-helm/ca-tls-certificates/rds-combined-ca-bundle.pem
+oc create secret generic mongodb-ca --from-file=$current_dir/5gcore-helm/ca-tls-certificates/rds-combined-ca-bundle.pem
 oc get secret
 cd 5gcore-helm
 echo "Deploying Open5G Core"
 helm install -f values.yaml 5gcore ./
-cd $current_dir
-echo "Creating Istio Ingress Virtual Service"
-oc create -f istio-manifests/open5gsweb.yaml
 echo "Enjoy The Open 5G Core!"
