5d0c9ac17f40ba6fea679dfafc0f500dcd96c679
Fatih Nar <fenar@yahoo.com>
2021-05-27 14:43:22 -0500

    Update 1-deploy5gran.sh

--- 1-deploy5gran.sh
+++ 1-deploy5gran.sh
@@ -9,4 +9,4 @@ cp templates/5gran-gnb-configmap.yaml.bak templates/5gran-gnb-configmap.yaml
 sed -e "s/<put-your-amf-service-ip-here>/$(<amf-ip sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' | tr -d '\n')/g" -i templates/5gran-gnb-configmap.yaml
 echo "gNB Config:" && cat templates/5gran-gnb-configmap.yaml
 helm install -f values.yaml 5gran ./
-echo "Enjoy The UERANSIM!"
+echo "Enjoy The 5GRAN!"
