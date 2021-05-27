fdc311feeb9e7b10f029a70c57c7ebc79e65ba87
fenar <fenar@jumpserver.narlabs.io>
2021-05-27 12:26:29 -0500

    helminazing ueransim partIII

--- 1-deployUERANSIM.sh
+++ 1-deployUERANSIM.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 #Author: fenar
-cd ueransim
+cd ueransim-helm
 ## gNB Section
 echo "Preparing gNB config"
 oc get services | grep amf-open5gs-sctp | awk '{print $3}' > amf-ip
 echo "AMF IP:" && cat amf-ip
-cp ueransim-gnb-configmap.yaml.bak ueransim-gnb-configmap.yaml
-sed -e "s/<put-your-amf-service-ip-here>/$(<amf-ip sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' | tr -d '\n')/g" -i ueransim-gnb-configmap.yaml
-echo "gNB Config:" && cat ueransim-gnb-configmap.yaml
-oc create -f ueransim-gnb-configmap.yaml
-oc create -f ueransim-ue-configmap.yaml
-oc create -f ueransim-deploy.yaml
+cp templates/ueransim-gnb-configmap.yaml.bak templates/ueransim-gnb-configmap.yaml
+sed -e "s/<put-your-amf-service-ip-here>/$(<amf-ip sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' | tr -d '\n')/g" -i templates/ueransim-gnb-configmap.yaml
+echo "gNB Config:" && cat templates/ueransim-gnb-configmap.yaml
+helm install -f values.yaml ueransim ./
 echo "Enjoy The UERANSIM!"
--- 8-deleteUERANSIM.sh
+++ 8-deleteUERANSIM.sh
@@ -2,10 +2,9 @@
 #Author: fenar
 echo -e "Wiping UERANSIM....\n"
 echo
-cd ueransim
+cd ueransim-helm
+rm amf-ip
 echo "Removing UERANSIM Deployment"
-oc delete -f ueransim-gnb-configmap.yaml
-oc delete -f ueransim-ue-configmap.yaml
-oc delete -f ueransim-deploy.yaml
-rm ueransim-gnb-configmap.yaml
+rm templates/ueransim-gnb-configmap.yaml
+helm uninstall ueransim
 echo "Bye"
