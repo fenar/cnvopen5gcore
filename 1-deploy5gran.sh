80a06442e1d7489c31167abf91a646bb077be69f
Fatih Nar <fenar@yahoo.com>
2021-05-27 15:50:57 -0500

    Update 1-deploy5gran.sh

--- 1-deploy5gran.sh
+++ 1-deploy5gran.sh
@@ -5,7 +5,7 @@ cd 5gran
 echo "Preparing gNB config"
 oc get services -n open5gcore | grep amf-open5gs-sctp | awk '{print $3}' > amf-ip
 echo "AMF IP:" && cat amf-ip
-cp templates/5gran-gnb-configmap-bk.yaml templates/5gran-gnb-configmap.yaml
+cp templates/5gran-gnb-configmap.bak templates/5gran-gnb-configmap.yaml
 sed -e "s/<put-your-amf-service-ip-here>/$(<amf-ip sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' | tr -d '\n')/g" -i templates/5gran-gnb-configmap.yaml
 echo "gNB Config:" && cat templates/5gran-gnb-configmap.yaml
 helm install -f values.yaml 5gran ./
