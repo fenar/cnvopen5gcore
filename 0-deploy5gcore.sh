6e38daf386ae27b74e8e58fa2c29578d132fd114
Fatih Nar <fenar@yahoo.com>
2021-06-16 18:19:46 -0500

    Update 0-deploy5gcore.sh

--- 0-deploy5gcore.sh
+++ 0-deploy5gcore.sh
@@ -6,10 +6,11 @@ echo -e "Adding project to service mesh member-roll\n"
 oc -n istio-system patch --type='json' smmr default -p '[{"op": "add", "path": "/spec/members", "value":["'"open5gcore"'"]}]'
 echo -e "Configuring privileged access - Sorry it is what is....\n"
 echo
-oc adm policy add-scc-to-user anyuid -z default 
-oc adm policy add-scc-to-user hostaccess -z default 
-oc adm policy add-scc-to-user hostmount-anyuid -z default 
-oc adm policy add-scc-to-user privileged -z default 
+#oc adm policy add-scc-to-user anyuid -z default 
+#oc adm policy add-scc-to-user hostaccess -z default 
+#oc adm policy add-scc-to-user hostmount-anyuid -z default 
+#oc adm policy add-scc-to-user privileged -z default 
+oc create -f scc.yaml
 current_dir=$PWD
 oc create secret generic mongodb-ca --from-file=$current_dir/5gcore/ca-tls-certificates/rds-combined-ca-bundle.pem
 oc get secret
