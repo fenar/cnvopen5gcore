b1b5bf238df5c541f9d7dda75de0b379b27b2317
Fatih Nar <fenar@yahoo.com>
2022-05-23 11:38:52 -0500

    Update deploy-federated5gcore.sh

--- 5gcore/mesh/deploy-federated5gcore.sh
+++ 5gcore/mesh/deploy-federated5gcore.sh
@@ -38,9 +38,12 @@ oc wait --for condition=Ready -n prod1-mesh smmr/default --timeout 300s
 log "Waiting for prod2-mesh installation to complete"
 oc wait --for condition=Ready -n prod2-mesh smmr/default --timeout 300s
 
-log "Installing AMF CNF service in prod2-mesh"
+log "Installing AMF, UPF CNF service in prod2-mesh"
 oc apply -n prod2-5gcore -f site2/amf-v2-configmap.yaml
 oc apply -n prod2-5gcore -f site2/amf-v2-deploy.yaml
+oc apply -n prod2-5gcore -f site2/upf-v2-configmap.yaml
+oc apply -n prod2-5gcore -f site2/upf-v2-deploy.yaml
+
 
 log "Installing Full 5gcore in prod1-mesh"
 ./site1/deploy-prod1-5gcore.sh
