29a816a6ca36533a42672df778d4707230ebc6dd
Fatih Nar <fenar@yahoo.com>
2022-05-23 09:36:16 -0500

    Update deploy-prod2mesh.sh

--- 5gcore/mesh/site2/deploy-prod2mesh.sh
+++ 5gcore/mesh/site2/deploy-prod2mesh.sh
@@ -7,34 +7,31 @@ log() {
   echo "##### $*"
 }
 
-log "Creating projects for prod1-mesh"
-oc new-project prod1-mesh || true
-oc new-project prod1-5gcore || true
+log "Creating projects for prod2-mesh"
+oc new-project prod2-mesh || true
+oc new-project prod2-5gcore || true
 
 log "Installing control plane for prod-mesh"
-oc apply -f prod1-smcp.yaml
-oc apply -f prod1-smmr.yaml
+oc apply -f prod2-smcp.yaml
+oc apply -f prod2-smmr.yaml
 
 log "Waiting for prod-mesh installation to complete"
-oc wait --for condition=Ready -n prod1-mesh smmr/default --timeout 300s
+oc wait --for condition=Ready -n prod2-mesh smmr/default --timeout 300s
 
 log "Installing 5gcore application in prod-mesh"
 ./0-deploy5gcore.sh
 
 log "Retrieving Istio CA Root certificates"
-PROD_MESH_CERT=$(oc get configmap -n prod1-mesh istio-ca-root-cert -o jsonpath='{.data.root-cert\.pem}' | sed ':a;N;$!ba;s/\n/\\\n    /g')
+PROD_MESH_CERT=$(oc get configmap -n prod2-mesh istio-ca-root-cert -o jsonpath='{.data.root-cert\.pem}' | sed ':a;N;$!ba;s/\n/\\\n    /g')
 
 log "Enabling federation for prod-mesh"
-sed "s:{{STAGE_MESH_CERT}}:$STAGE_MESH_CERT:g" prod1-stage-mesh-ca-root-cert.yaml | oc apply -f -
-oc apply -f prod1-smp.yaml
-oc apply -f prod1-iss.yaml
+sed "s:{{STAGE_MESH_CERT}}:$STAGE_MESH_CERT:g" prod2-stage-mesh-ca-root-cert.yaml | oc apply -f -
+oc apply -f prod2-smp.yaml
+oc apply -f prod2-iss.yaml
 
 log "Installing VirtualService for prod-mesh"
-oc apply -n prod1-5gcore -f prod-vs-mirror-details.yaml
+oc apply -n prod2-5gcore -f prod2-vs-mirror-details.yaml
 
 log "INSTALLATION COMPLETE"
-oc -n prod1-mesh get servicemeshpeer prod2-mesh -o json | jq .status
-oc -n prod1-mesh get importedservicesets prod2-mesh -o json | jq .status
-
-
-
+oc -n prod2-mesh get servicemeshpeer prod1-mesh -o json | jq .status
+oc -n prod2-mesh get importedservicesets prod1-mesh -o json | jq .status
