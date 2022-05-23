d0ed881010a30d99405a19bb42bb15d4c9f2a6da
Fatih Nar <fenar@yahoo.com>
2022-05-23 12:11:58 -0500

    Update deploy-federated5gcore.sh

--- 5gcore/mesh/deploy-federated5gcore.sh
+++ 5gcore/mesh/deploy-federated5gcore.sh
@@ -7,6 +7,7 @@ log() {
   echo "##### $*"
 }
 
+# to do: switch context: oc config use-context prod1-cluster
 log "Creating projects for prod-mesh"
 oc new-project prod1-mesh || true
 oc new-project prod1-5gcore || true
@@ -19,6 +20,7 @@ log "Installing control plane for prod-mesh"
 oc apply -f site1/smcp.yaml
 oc apply -f site1/smmr.yaml
 
+# to do: switch context: oc config use-context prod2-cluster
 log "Creating projects for prod2-mesh"
 oc new-project prod2-mesh || true
 oc new-project prod2-5gcore|| true
@@ -32,9 +34,11 @@ log "Installing control plane for prod2-mesh"
 oc apply -f site2/smcp.yaml
 oc apply -f site2/smmr.yaml
 
+# to do: switch context: oc config use-context prod1-cluster
 log "Waiting for prod1-mesh installation to complete"
 oc wait --for condition=Ready -n prod1-mesh smmr/default --timeout 300s
 
+# to do: switch context: oc config use-context prod2-cluster
 log "Waiting for prod2-mesh installation to complete"
 oc wait --for condition=Ready -n prod2-mesh smmr/default --timeout 300s
 
@@ -48,10 +52,13 @@ oc apply -n prod2-5gcore -f site2/upf-v2-deploy.yaml
 log "Installing Full 5gcore in prod1-mesh"
 ./site1/deploy-prod1-5gcore.sh
 
+# to do: switch context: oc config use-context prod1-cluster
 log "Retrieving Istio CA Root certificates"
 PROD1_MESH_CERT=$(oc get configmap -n prod1-mesh istio-ca-root-cert -o jsonpath='{.data.root-cert\.pem}' | sed ':a;N;$!ba;s/\n/\\\n    /g')
+# to do: switch context: oc config use-context prod2-cluster
 PROD2_MESH_CERT=$(oc get configmap -n prod2-mesh istio-ca-root-cert -o jsonpath='{.data.root-cert\.pem}' | sed ':a;N;$!ba;s/\n/\\\n    /g')
 
+# to do: switch context: oc config use-context prod1-cluster
 log "Enabling federation for prod-mesh"
 sed "s:{{PROD2_MESH_CERT}}:$PROD2_MESH_CERT:g" prod1-mesh/prod2-mesh-ca-root-cert.yaml | oc apply -f -
 oc apply -f site1/smp.yaml
