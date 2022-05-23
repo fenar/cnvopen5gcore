94fdbf10f8868035a0ae5de264c083b1bfe75c51
Fatih Nar <fenar@yahoo.com>
2022-05-23 10:29:55 -0500

    Create deploy-federated5gcore.sh

---
+++ 5gcore/mesh/deploy-federated5gcore.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+set -e
+
+log() {
+  echo
+  echo "##### $*"
+}
+
+log "Creating projects for prod-mesh"
+oc new-project prod1-mesh || true
+oc new-project prod1-5gcore || true
+oc adm policy add-scc-to-user anyuid -z default 
+oc adm policy add-scc-to-user hostaccess -z default 
+oc adm policy add-scc-to-user hostmount-anyuid -z default 
+oc adm policy add-scc-to-user privileged -z default 
+
+log "Installing control plane for prod-mesh"
+oc apply -f site1/smcp.yaml
+oc apply -f site1/smmr.yaml
+
+log "Creating projects for stage-mesh"
+oc new-project prod2-mesh || true
+oc new-project prod2-5gcore|| true
+
+oc adm policy add-scc-to-user anyuid -z default 
+oc adm policy add-scc-to-user hostaccess -z default 
+oc adm policy add-scc-to-user hostmount-anyuid -z default 
+oc adm policy add-scc-to-user privileged -z default 
+
+log "Installing control plane for stage-mesh"
+oc apply -f site2/smcp.yaml
+oc apply -f site2/smmr.yaml
+
+log "Waiting for prod1-mesh installation to complete"
+oc wait --for condition=Ready -n prod1-mesh smmr/default --timeout 300s
+
+log "Waiting for prod2-mesh installation to complete"
+oc wait --for condition=Ready -n prod2-mesh smmr/default --timeout 300s
+
+log "Installing AMF v2 service in prod2-mesh"
+oc apply -n prod2-5gcore -f site2/prod2-amf-v2.yaml
+
+log "Installing 5gcore in prod1-mesh"
+./site1/deploy-prod1-5gcore.sh
+
+log "Retrieving Istio CA Root certificates"
+PROD1_MESH_CERT=$(oc get configmap -n prod1-mesh istio-ca-root-cert -o jsonpath='{.data.root-cert\.pem}' | sed ':a;N;$!ba;s/\n/\\\n    /g')
+PROD2_MESH_CERT=$(oc get configmap -n prod2-mesh istio-ca-root-cert -o jsonpath='{.data.root-cert\.pem}' | sed ':a;N;$!ba;s/\n/\\\n    /g')
+
+log "Enabling federation for prod-mesh"
+sed "s:{{PROD2_MESH_CERT}}:$PROD2_MESH_CERT:g" prod1-mesh/prod2-mesh-ca-root-cert.yaml | oc apply -f -
+oc apply -f site1/prod1-smp.yaml
+oc apply -f site1/prod1-iss.yaml
+
+log "Enabling federation for prod2-mesh"
+sed "s:{{PROD1_MESH_CERT}}:$PROD1_MESH_CERT:g" prod2-mesh/prod1-mesh-ca-root-cert.yaml | oc apply -f -
+oc apply -f site2/prod2-smp.yaml
+oc apply -f site2/prod2-ess.yaml
+
+log "Installing VirtualService for prod-mesh"
+oc apply -n prod1-5gcore -f site1/prod1-vs-mirror-details.yaml
+
+log "INSTALLATION COMPLETE
+#Run the following command in the prod-mesh to check the connection status:
+  oc -n prod1-mesh get servicemeshpeer prod2-mesh -o json | jq .status
+#Run the following command to check the connection status in stage-mesh:
+  oc -n prod2-mesh get servicemeshpeer prod1-mesh -o json | jq .status
+#Check if services from stage-mesh are imported into prod-mesh:
+  oc -n prod1-mesh get importedservicesets prod2-mesh -o json | jq .status
+#Check if services from stage-mesh are exported:
+  oc -n prod2-mesh get exportedservicesets prod1-mesh -o json | jq .status
+"
